cbd655afd2566846dd4265f7f6ac53a20808ab68
Aidan Anderson <aidan@floqast.com>
2017-07-28 20:30:56 -0700

    Removed redux, bare bones react

--- src/index.js
+++ src/index.js
@@ -7,17 +7,14 @@ import ReactDOM from 'react-dom';
  */
 import './styles/main.scss';
 
-/**
- * Both configureStore and Root are required conditionally.
- * See configureStore.js and Root.js for more details.
- */
-import { configureStore } from './store/configureStore';
-import { Root } from './containers/Root';
+const Root = () => {
+    return (
+        <div>testing</div>
+    );
+};
 
-
-const store = configureStore();
 
 ReactDOM.render(
-  <Root store={store} />,
+  <Root />,
   document.getElementById('root')
 );
